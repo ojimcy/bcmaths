c79fd19ac536dce8ee0f94807ea06f95c0751066
ojimcy <ojimcy247@gmail.com>
2022-04-04 12:38:48 +0100

    read from env

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const Handlebars = require('handlebars');
 const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+require('dotenv').config()
 
 //database
 const db = require('./config/db');
@@ -42,6 +43,6 @@ app.use('/order', require('./routes/index'))
 
 
 
-const PORT = process.env.PORT || 5050;
+const PORT = process.env.PORT;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
